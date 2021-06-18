6f111187f63f1aa4fa8755f7216e093cf853c552
bhavikx <64426653+bhavikx@users.noreply.github.com>
2021-06-18 15:17:10 +0530

    Update settings.py

--- sofap/settings.py
+++ sofap/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6y_enk!o5txynoq!wgr_=2r#@lf^irue)cy77!zy7fdk6%d7ne'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
